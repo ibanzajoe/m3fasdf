db888b1021885ecb47ef6f4261ce33e7af62ce56
jaequery <jaequery@gmail.com>
2016-02-26 17:04:23 -0800

    added company page

--- app/admin/app.rb
+++ app/admin/app.rb
@@ -264,6 +264,11 @@ module Honeybadger
     # end setting routes
 
     ### end of routes ###
+    
+    error Sinatra::NotFound do
+      content_type 'text/plain'
+      [404, 'Not Found']
+    end
 
 
     ### utility methods ###
--- app/site/app.rb
+++ app/site/app.rb
@@ -213,6 +213,13 @@ module Honeybadger
       render "about"
     end
 
+    error Sinatra::NotFound do
+      content_type 'text/plain'
+      [404, 'Not Found']
+    end
+
+
+    
   end
 
 end
