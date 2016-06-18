ead2f3bbe866fc638b647ae83330426fa54246f8
jaequery <jaequery@gmail.com>
2016-06-17 20:42:57 -0700

    made withdrawal accessible to over

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,5 @@ parentdir="$(dirname "$dir")"
 app=${parentdir##*/}
 dest=$1
 dest="jae@x.hakberry.com"
-ssh $dest "cd ~/sites/$app/bin && ./psql_dump.sh production"
+#ssh $dest "cd ~/sites/$app/bin && ./psql_dump.sh production"
 rsync --exclude="volumes" --exclude=".git" -avzr $parentdir $dest:~/sites/ && ssh $dest "cd ~/sites/$app && docker-compose stop && docker-compose -f docker-compose.live.yml up && docker-compose logs"
